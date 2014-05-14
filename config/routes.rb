933cd08f60c3760a3005fb25f4dbd511b2242c2c
Zeljko <zeljko@zwr.fi>
2014-05-14 21:19:41 +0300

    Add categories routes
    These seem to have been forgotten by scaffold generator
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :product_packeds
 
   resources :products
+  
+  resources :categories do
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
