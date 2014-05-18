6c53972217e26755c41633a52fbc41c34a5804b7
Oscar Sanhueza <oscarsanhueza@gmail.com>
2014-05-18 16:13:37 +0300

    define return of main_category on MainController
    Signed-off-by: Oscar Sanhueza <oscarsanhueza@gmail.com>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
+  before_action :set_main_category
+
   def get
+  end
+private
+  def set_main_category
+    @main_category = Category.first
   end
 end
