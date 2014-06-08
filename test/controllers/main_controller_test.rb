702d0e3211c74191f514898df57f3267c0d8f673
Zeljko <zeljko@zwr.fi>
2014-06-08 17:24:52 +0300

    Fix test: need factories
    Test was missing data in the database.
    
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class MainControllerTest < ActionController::TestCase
+=begin
+  setup do
+    Product.all.delete
+    Category.all.delete
+    @product = create(:product)
+    @category = create(:category)
+  end
+
+  teardown do
+    Product.all.delete
+    Category.all.delete
+  end
+=end
   test "should get get" do
     get :get
     assert_response :success
